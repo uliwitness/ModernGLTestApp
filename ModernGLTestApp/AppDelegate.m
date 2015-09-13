428512340bd6be91e969777d74d3930a890cbef9
Uli Kusterer <ulivc@zathras.de>
2015-09-13 18:59:27 +0200

    Center main window on startup.

--- ModernGLTestApp/AppDelegate.m
+++ ModernGLTestApp/AppDelegate.m
@@ -16,7 +16,7 @@
 @implementation AppDelegate
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification {
-    // Insert code here to initialize your application
+    [self.window center];
 }
 
 - (void)applicationWillTerminate:(NSNotification *)aNotification {
