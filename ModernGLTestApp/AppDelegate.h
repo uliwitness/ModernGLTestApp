2b00990b1712d64c8816ce4b61dc890d64dc8253
Uli Kusterer <ulivc@zathras.de>
2015-09-13 13:16:12 +0200

    Initial Commit

---
+++ ModernGLTestApp/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  ModernGLTestApp
+//
+//  Created by Uli Kusterer on 13/09/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AppDelegate : NSObject <NSApplicationDelegate>
+
+
+@end
+
---
+++ ModernGLTestApp/main.m
@@ -0,0 +1,13 @@
+//
+//  main.m
+//  ModernGLTestApp
+//
+//  Created by Uli Kusterer on 13/09/15.
+//  Copyright (c) 2015 Uli Kusterer. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+int main(int argc, const char * argv[]) {
+    return NSApplicationMain(argc, argv);
+}
